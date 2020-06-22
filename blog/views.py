bdf27d4799bb7ab4ae97fd677b01cb73b9ce27e8
roman <rxp777@student.bham.ac.uk>
2020-06-22 08:29:23 +0100

    added edit post and admin authentication to edit

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone # for queryset filter
 from .models import Post # include the model written previously
 from .forms import PostForm
@@ -13,5 +13,30 @@ def post_detail(request, pk):
     return render(request, 'blog/post_detail.html', {'post': post})
 
 def post_new(request):
-    form = PostForm()
-    return render(request, 'blog/post_edit.html', {'form':form})
+    if request.method == "POST":# Accessing page for the first time an we want a blank form
+        form = PostForm(request.POST)
+        if form.is_valid():# Checking if the form is correct
+            # Saving it
+            post = form.save(commit=False)# false means we don't want to save the post model yet
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)# redirects us to the new post
+    else:# we go back to the view with all the form data we just typed
+        form = PostForm()
+    return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'blog/post_edit.html', {'form': form})
