d2016ded28dc9d182d5b0677a8db082ea906c106
roman <rxp777@student.bham.ac.uk>
2020-07-13 16:10:20 +0100

    added rounded background to menu, posts, education and work

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import Post, CV
+from .models import Post, CV, Work, Education
 
 admin.site.register(Post) # makes the model visible in the admin page
 admin.site.register(CV)
+admin.site.register(Work)
+admin.site.register(Education)
 # Register your models here.
